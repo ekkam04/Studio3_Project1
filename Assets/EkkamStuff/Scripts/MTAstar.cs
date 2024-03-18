19cb11a5f6ec2b13130abd1066f96f52cbf2bb03
ekkam04 <ekkam04yt@gmail.com>
2024-03-18 12:38:17 +0400

    Started multithreading and improved player cam

---
+++ Assets/EkkamStuff/Scripts/MTAstar.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Ekkam
+{
+    public class MTAstar : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/EkkamStuff/Scripts/PathfindingJob.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Ekkam
+{
+    public class PathfindingJob : MonoBehaviour
+    {
+        
+    }
+}
